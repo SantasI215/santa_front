fc3ce4566967c6472abce52f73c70240eff1df2b
lifeo <lifeonobl1vion@gmail.com>
2024-11-12 13:18:20 +0300

    start

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import '@/styles/reset.css';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+      <>
+        <Head>
+          {/*<link rel="icon" href="/Logo.svg" />*/}
+        </Head>
+        <Component {...pageProps} />
+      </>
+  )
 }
